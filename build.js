2f5cbe137e1c33f40917bd6085541e2b418d7ac8
deltasampler <deltasampler@gmail.com>
2025-07-10 19:43:20 +0300

    update build config

--- build.js
+++ build.js
@@ -14,9 +14,10 @@ const config = {
 };
 
 function build() {
+    const commands = [];
+    const env = {};
+
     const platform = os.platform();
-    const env = Object.assign({}, process.env);
-    const commands = []
 
     if (platform === "win32") {
         const windows = config.windows;
@@ -24,29 +25,37 @@ function build() {
         commands.push(`"${windows.vsdevcmd}"`);
         commands.push(`cmake --preset "${windows.preset}"`);
         commands.push(`ninja -C "out/build/${windows.preset}/"`);
-        commands.push(`cd out/build/${windows.preset}/${config.name}/`);
+        commands.push(`cd out/build/${windows.preset}/`);
         commands.push(`.\\${config.name}.exe`);
     } else if (platform === "linux") {
-        const linux = config.windows;
+        const linux = config.linux;
 
         commands.push(`cmake --preset "${linux.preset}"`);
         commands.push(`ninja -C "out/build/${linux.preset}/"`);
-        commands.push(`cd out/build/${linux.preset}/${config.name}/`);
+        commands.push(`cd out/build/${linux.preset}/`);
         commands.push(`./${config.name}`);
 
         env.VCPKG_ROOT = linux.vcpkg_root;
     } else {
-        console.error(`Building for ${platform} is not supported`);
+        console.error(`Building for "${platform}" is not supported`);
 
         return;
     }
 
-    const command = commands.join(" && ");
-
     try {
-        execSync(command, {stdio: "inherit", env, shell: true});
-    } catch (error) {
-        console.error(error.message);
+        execSync(
+            commands.join(" && "),
+            {
+                env: {
+                    ...process.env,
+                    ...env
+                },
+                shell: true,
+                stdio: "inherit"
+            }
+        );
+    } catch (e) {
+        console.error(e.message);
     }
 }
 
